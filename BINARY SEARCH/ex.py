ef0815f19d3a3952e26112315b399b5d7f5535b3
HARSHIT YADAV <govindchandrayadav7379@gmail.com>
2023-02-28 23:09:31 +0530

    initial

---
+++ BINARY SEARCH/ex.py
@@ -0,0 +1,13 @@
+class student:
+    def __init__(self): 
+        self.name = "HARSHIT"
+        self.roll = 60
+    def disp(baby,x):
+        print(baby.name)
+        print(baby.roll)
+
+s=student()
+print(s.name)
+print(s.roll)
+print(s.disp(10))
+   
---
+++ BINARY SEARCH/first.py
@@ -0,0 +1,5 @@
+class Student:
+    '''harshit'''## string document
+    # variable / instance / static / local
+    # method(behaviour) / instance / static / class
+print(Student.__doc__)
